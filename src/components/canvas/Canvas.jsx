c9e43a45d2fb6689d4ffb5b0546d84e6e0b88d38
CheoR <cheo.roman+github@gmail.com>
2021-11-21 22:42:28 -0600

    build: remove and replace default app with canvas.

---
+++ src/components/canvas/Canvas.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import styles from './canvas.module.css';
+
+export const Canvas = () => <canvas className={styles.canvas} />;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Creanvas } from './components/Creanvas';
+
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Creanvas />
   </React.StrictMode>,
   document.getElementById('root')
 );
