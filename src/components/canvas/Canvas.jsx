26b8365e99e68d1ad6a4ebc43a3c0214f1b01dfa
CheoR <cheo.roman+github@gmail.com>
2021-11-22 15:46:18 -0600

    build: canvas renders with minial usage. User can draw with black brush.

--- src/components/canvas/Canvas.jsx
+++ src/components/canvas/Canvas.jsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-import styles from './canvas.module.css';
+// import styles from './canvas.module.css';
 
-export const Canvas = () => <canvas className={styles.canvas} />;
+export const Canvas = () => {
+  const canvasRef = useRef(null);
+  const ctx = useRef(null);
+
+  const [mouseDown, setMouseDown] = useState(false);
+  const [lastPosition, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      ctx.current = canvasRef.current.getContext('2d');
+    }
+  }, []);
+
+  const draw = useCallback((x, y) => {
+    if (mouseDown) {
+      ctx.current.beginPath();
+      ctx.current.strokeStyle = 'black';
+      ctx.current.lineWidth = 5;
+      ctx.current.lineJoin = 'round';
+      ctx.current.moveTo(lastPosition.x, lastPosition.y);
+      ctx.current.lineTo(x, y);
+      ctx.current.closePath();
+      ctx.current.stroke();
+
+      setPosition({
+        x,
+        y,
+      });
+    }
+  }, [lastPosition, mouseDown, setPosition]);
+
+  const onMouseDown = (e) => {
+    setPosition({
+      x: e.pageX,
+      y: e.pageY,
+    });
+    setMouseDown(true);
+  };
+
+  const onMouseUp = () => setMouseDown(false);
+
+  const onMouseMove = (e) => draw(e.pageX, e.pageY);
+
+  return (
+    <canvas
+      style={{
+        border: '2px solid black',
+      }}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      ref={canvasRef}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+      onMouseLeave={onMouseUp}
+      onMouseMove={onMouseMove}
+    />
+  );
+};
